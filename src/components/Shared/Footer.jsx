21a0252c67a42af33123dc362922fdcab77129b3
tarifalhasan <tarifalhasan999@gmail.com>
2024-12-02 12:40:11 +0600

    update footer

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -1,34 +1,46 @@
 import { FaDiscord, FaInstagram, FaTelegramPlane } from "react-icons/fa";
-import Container from "./Container";
 import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <footer className="bg-[#464343]">
-      <Container>
+      <div className="container">
         {/* Newsletter */}
-        <div className="pt-14 flex justify-center items-center">
-          <div className="flex">
+        <div className="pt-16 ">
+          <form className="mx-auto rounded-lg  bg-white flex relative max-w-md gap-x-4">
+            <label htmlFor="email-address" className="sr-only">
+              Email address
+            </label>
             <input
-              type="text"
-              className="border px-4 py-2 text-lg rounded-l-md rounded-r-none"
+              id="email-address"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required=""
+              className="min-w-0 flex-auto rounded-md border-0  px-3.5 py-2 text-black shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
+              placeholder="Enter your email"
             />
-            <button className="bg-primary text-white px-6 py-2 text-lg rounded-r-md rounded-l-none">
+            <button
+              type="submit"
+              className="flex-none rounded-lg bg-primary px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            >
               Get Started
             </button>
-          </div>
+          </form>
         </div>
 
         <div className="mx-auto space-y-8 px-4 py-16 sm:px-6 lg:space-y-16">
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:col-span-5 lg:grid-cols-5 ">
               <div>
-                <p className="font-[700] text-[24px] text-white">FAQ</p>
-                <ul className="mt-6 space-y-4 font-[500] text-[20px]">
+                <p className="font-inter text-lg lg:text-xl text-white font-bold">
+                  FAQ
+                </p>
+                <ul className="text-base lg:text-lg font-normal hover:text-primary transition-all duration-500 mt-6">
                   <li>
                     <a
                       href="#"
-                      className="transition hover:opacity-75 text-gray-200"
+                      className=" hover:text-primary transition-colors duration-500 text-gray-200"
                     >
                       Contact Us
                     </a>
@@ -36,7 +48,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="#"
-                      className="transition hover:opacity-75 text-gray-200"
+                      className=" hover:text-primary transition-colors duration-500 text-gray-200"
                     >
                       About Cineflicks
                     </a>
@@ -45,12 +57,14 @@ const Footer = () => {
               </div>
 
               <div>
-                <p className="font-[700] text-[24px] text-white">My Account</p>
-                <ul className="mt-6 space-y-4 font-[500] text-[20px]">
+                <p className="font-inter text-lg lg:text-xl text-white font-bold">
+                  My Account
+                </p>
+                <ul className="text-base lg:text-lg font-normal hover:text-primary transition-all duration-500 mt-6">
                   <li>
                     <a
                       href="#"
-                      className="transition hover:opacity-75 text-gray-200"
+                      className=" hover:text-primary transition-colors duration-500 text-gray-200"
                     >
                       Premium Upgrades
                     </a>
@@ -58,7 +72,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="#"
-                      className="transition hover:opacity-75 text-gray-200"
+                      className=" hover:text-primary transition-colors duration-500 text-gray-200"
                     >
                       Plans & Pricing
                     </a>
@@ -67,14 +81,14 @@ const Footer = () => {
               </div>
 
               <div>
-                <p className="font-[700] text-[24px] text-white">
+                <p className="font-inter text-lg lg:text-xl text-white font-bold">
                   Supported Devices
                 </p>
-                <ul className="mt-6 space-y-4 font-[500] text-[20px]">
+                <ul className="text-base lg:text-lg font-normal hover:text-primary transition-all duration-500 mt-6">
                   <li>
                     <a
                       href="#"
-                      className="transition hover:opacity-75 text-gray-200"
+                      className=" hover:text-primary transition-colors duration-500 text-gray-200"
                     >
                       Press
                     </a>
@@ -82,7 +96,7 @@ const Footer = () => {
                   <li>
                     <a
                       href="#"
-                      className="transition hover:opacity-75 text-gray-200"
+                      className=" hover:text-primary transition-colors duration-500 text-gray-200"
                     >
                       Investor Relations
                     </a>
@@ -106,26 +120,46 @@ const Footer = () => {
                   />
                 </div>
               </div>
-              <div className="grid gap-y-4 justify-items-end text-5xl gap-4">
-                <div className="text-black">
-                  <div className="bg-primary p-6 rounded-full w-20 h-20 flex items-center justify-center my-3">
-                    <FaDiscord className="text-6xl" />
-                  </div>
-                  <div className="bg-primary p-6 rounded-full w-20 h-20 flex items-center justify-center my-3">
-                    <FaTelegramPlane />
-                  </div>
-                  <div className="bg-primary p-6 rounded-full w-20 h-20 flex items-center justify-center my-3">
-                    <FaXTwitter />
-                  </div>
-                  <div className="bg-primary p-6 rounded-full w-20 h-20 flex items-center justify-center my-3">
-                    <FaInstagram />
-                  </div>
+              <div className="grid  gap-y-4 justify-items-end text-5xl gap-4">
+                <div className="text-black flex flex-row lg:flex-col justify-center lg:justify-stretch gap-6 lg:gap-0">
+                  <a
+                    href="http://"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-primary hover:bg-opacity-60 transition-all duration-700  rounded-full w-16 h-16 flex items-center justify-center my-3"
+                  >
+                    <FaDiscord className="text-2xl" />
+                  </a>
+                  <a
+                    href="http://"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-primary hover:bg-opacity-60 transition-all duration-700  rounded-full w-16 h-16 flex items-center justify-center my-3"
+                  >
+                    <FaTelegramPlane className="text-2xl" />
+                  </a>
+                  <a
+                    href="http://"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-primary hover:bg-opacity-60 transition-all duration-700  rounded-full w-16 h-16 flex items-center justify-center my-3"
+                  >
+                    <FaXTwitter className="text-2xl" />
+                  </a>
+                  <a
+                    href="http://"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-primary hover:bg-opacity-60 transition-all duration-700  rounded-full w-16 h-16 flex items-center justify-center my-3"
+                  >
+                    <FaInstagram className="text-2xl" />
+                  </a>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </Container>
+      </div>
       <p className="py-[56px] text-center bg-primary font-[20px ]">
         &copy; 2024 Cineflicks All Rights Reserved.
       </p>
